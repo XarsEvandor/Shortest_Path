6314018002a2d6c2fdf7910b6a9e1532047f8c5c
XarsEvandor <giorgos_katsonis@hotmail.com>
2021-04-09 12:55:24 +0300

    Small Corrections

--- graphs.py
+++ graphs.py
@@ -1,12 +1,10 @@
 import networkx as nx
-import matplotlib.pyplot as plt
-import math
 
 G = nx.DiGraph()
-G.add_node(1, name = "Earth", visited = False)
-G.add_node(2, name = "Jupiter", visited = False)
-G.add_node(3, name = "Saturn", visited = False)
-G.add_node(4, name = "Ceres", visited = False)
+G.add_node(1, name="Earth", visited=False)
+G.add_node(2, name="Jupiter", visited=False)
+G.add_node(3, name="Saturn", visited=False)
+G.add_node(4, name="Ceres", visited=False)
 G.add_edge(1, 2, weight=637092)
 G.add_edge(1, 3, weight=1287054)
 G.add_edge(1, 4, weight=264400)
@@ -21,6 +19,8 @@ planet_name = nx.get_node_attributes(G, 'name')
 planet_visited = nx.get_node_attributes(G, 'visited')
 
 node_num = nx.number_of_nodes(G)
+
+
 # print(node_num)
 
 def bruteForce():
@@ -29,48 +29,48 @@ def bruteForce():
     minL = 0
     king = 0
 
-    #Brute force method.
+    # Brute force method.
     for n in range(2, node_num + 1):
         for k in range(1, node_num + 1):
-            if (n == k):
+            if n == k:
                 continue
-            if (k == 1):
+            if k == 1:
                 continue
             for l in range(1, node_num + 1):
-                if (n == k or n == l or k == l):
+                if n == k or n == l or k == l:
                     continue
-                if(l == 1):
+                if l == 1:
                     continue
                 try:
                     total_weight = G[1][n]["weight"] + G[n][k]["weight"] + G[k][l]["weight"]
                 except:
                     continue
 
-                if (total_weight < king):
+                if total_weight < king:
                     king = total_weight
                     minN = n
                     minK = k
                     minL = l
-                #Combination Unique to the first iteration.
-                elif (n == 2 and k == 3 and l == 4):
+                # Combination Unique to the first iteration.
+                elif n == 2 and k == 3 and l == 4:
                     king = total_weight
                     minN = n
                     minK = k
                     minL = l
                 print(planet_name[1] + " -> " + planet_name[n]
-                + " -> " + planet_name[k] + " -> " + planet_name[l] + "     Weight: " + str(total_weight))
+                      + " -> " + planet_name[k] + " -> " + planet_name[l] + "     Weight: " + str(total_weight))
 
     print("The optimal path is " + planet_name[1] + " -> " + planet_name[minN]
-        + " -> " + planet_name[minK] + " -> " + planet_name[minL])
+          + " -> " + planet_name[minK] + " -> " + planet_name[minL])
 
     txt = "With a total distance of: {king:,} km"
-    print(txt.format(king = king))
+    print(txt.format(king=king))
 
 
-#Nearest neighbor method
+# Nearest neighbor method
 
 def shortestNeighbor():
-    #We always start from earth
+    # We always start from earth
     nextStop = 1
     kinglist = []
     weight_sum = 0
@@ -86,24 +86,24 @@ def shortestNeighbor():
                 neighborlist.append(next(iterHolder))
             except StopIteration:
                 break
-            
-        #Set the king as the first non visited planet out of the neighbors of nextStop
+
+            # Set the king as the first non visited planet out of the neighbors of nextStop
             king = next(
                 filter(lambda planet: not planet_visited[planet], neighborlist), None)
         for k in neighborlist:
-            #Avoid same planet travel
-            if (nextStop == k or nextStop == king):
+            # Avoid same planet travel
+            if nextStop == k or nextStop == king:
                 continue
-            #Avoid visited planets
-            if (planet_visited[k]):
+            # Avoid visited planets
+            if planet_visited[k]:
                 continue
-            #Checks for the shortest path
-            if (G[nextStop][king]["weight"] > G[nextStop][k]["weight"]):
+            # Checks for the shortest path
+            if G[nextStop][king]["weight"] > G[nextStop][k]["weight"]:
                 king = k
 
         print(planet_name[nextStop] + " -> " + planet_name[king])
         weight_sum += G[nextStop][king]["weight"]
-        #Mark the planet as visited
+        # Mark the planet as visited
         planet_visited[nextStop] = True
         # The next stop is the shortest neighbor of the previous nextStop
         nextStop = king
